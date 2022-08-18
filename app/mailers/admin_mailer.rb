c8534c0fdead594dc04a984a93df65a45f1704aa
Freigth <cosiconeal@gmail.com>
2022-08-18 16:24:58 +0800

    init

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,10 +1,10 @@
 class AdminMailer < ApplicationMailer
-  default from: DEFAULT_FROM
+  default from: 'rails69@gmail.com'
   layout 'mailer'
 
   def new_user_waiting_for_approval(email)
     @email = email
     @url = 'https://secret-coast-88273.herokuapp.com/'
-    mail(to: 'admin@email.com', subject: 'New user awaiting admin approval')
+    mail(to: 'rails69@gmail.com', subject: 'New user awaiting admin approval')
   end
 end
