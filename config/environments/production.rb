fb20ae2b9e6d9fd0b07d0596984e508caca22b1f
Freigth <cosiconeal@gmail.com>
2022-08-21 21:42:40 +0800

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.railsmailer69@gmail.com',
+    address: 'smtp.@gmail.com',
     port: 587,
     domain: 'gmail.com',
     user_name: 'railsmailer69@gmail.com',
