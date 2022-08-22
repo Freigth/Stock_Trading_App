d296de8915b6a2dce8ea3e2c30347b1a4c4cb6b1
Freigth <cosiconeal@gmail.com>
2022-08-22 17:35:21 +0800

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,14 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # !
-  config.action_mailer.default_url_options = { host: 'protected-tor-42274.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'immense-beach-69182.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'protected-tor-42274.herokuapp.com',
+    domain: 'immense-beach-69182.herokuapp.com',
     user_name: 'railsmailer69@gmail.com',
     password: 'blckohuuyuddxxez',
     authentication: 'plain',
