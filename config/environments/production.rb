f587c47c7b0f4d4da651b8e3e239b683a896c1d8
Freigth <cosiconeal@gmail.com>
2022-08-22 17:32:17 +0800

    please save

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # !
-  config.action_mailer.default_url_options = { host: 'protected-tor-42274.herokuapp.comm' }
+  config.action_mailer.default_url_options = { host: 'protected-tor-42274.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
